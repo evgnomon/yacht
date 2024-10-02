2022a8894275a8c675e6fa229b3c93860877cfc4
Hamed Ghasemzadeh <hg@evgnomon.org>
2024-10-02 13:29:53 +0200

    Pass args to ansible-playbook (#2)

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-ansible-playbook -i "${INPUT_INVENTORY}" "${INPUT_PLAYBOOK}" -e 'ansible_python_interpreter=/opt/python/bin/python3'
+export ANSIBLE_STDOUT_CALLBACK=yaml
+ansible-playbook -i "${INPUT_INVENTORY}" "${INPUT_PLAYBOOK}" -v -e 'ansible_python_interpreter=/opt/python/bin/python3' ${INPUT_ARGS}
