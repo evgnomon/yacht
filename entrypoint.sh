34c8d70f40beebf5bc52056e56e99883a3deda94
Hamed Ghasemzadeh <hg@evgnomon.org>
2024-10-05 15:43:46 +0200

    Read Github secrets (#3)

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,19 @@
-#!/bin/sh
+#!/bin/bash
 
 export ANSIBLE_STDOUT_CALLBACK=yaml
-ansible-playbook -i "${INPUT_INVENTORY}" "${INPUT_PLAYBOOK}" -v -e 'ansible_python_interpreter=/opt/python/bin/python3' ${INPUT_ARGS}
+
+if [ -z "$INPUT_VAULT_PASS" ]; then
+  ansible-playbook -v \
+    -e 'ansible_python_interpreter=/opt/python/bin/python3'\
+    -i "$INPUT_INVENTORY" \
+    "$INPUT_PLAYBOOK" \
+    $INPUT_ARGS
+  exit $?
+fi
+
+ANSIBLE_VAULT_PASSWORD_FILE=<(echo "$INPUT_VAULT_PASS") INPUT_VAULT_FILE=<(echo "$INPUT_VAULT")\
+  ansible-playbook -v \
+  -e 'ansible_python_interpreter=/opt/python/bin/python3'\
+  -i "$INPUT_INVENTORY" \
+  "$INPUT_PLAYBOOK" \
+  $INPUT_ARGS
