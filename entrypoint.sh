5537cd04e35031526cea0345df9f844b21d5b50c
Hamed Ghasemzadeh <hg@evgnomon.org>
2024-10-05 09:13:27 +0200

    read vault info

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,18 @@
-#!/bin/sh
+#!/bin/bash
 
 export ANSIBLE_STDOUT_CALLBACK=yaml
-ansible-playbook -i "${INPUT_INVENTORY}" "${INPUT_PLAYBOOK}" -v -e 'ansible_python_interpreter=/opt/python/bin/python3' ${INPUT_ARGS}
+
+if [ -z "$INPUT_VAULT_PASS" ]; then
+    ansible-playbook -i "${INPUT_INVENTORY}"\
+    "${INPUT_PLAYBOOK}" -v \
+    -e 'ansible_python_interpreter=/opt/python/bin/python3'\
+    ${INPUT_ARGS}
+  exit $?
+fi
+
+ANSIBLE_VAULT_PASSWORD_FILE=<(echo "$INPUT_VAULT_PASS")\
+  ansible-playbook -i "${INPUT_INVENTORY}"\
+  --vault-password-file <(echo "$INPUT_VAULT") \
+  "${INPUT_PLAYBOOK}" -v \
+  -e 'ansible_python_interpreter=/opt/python/bin/python3'\
+  ${INPUT_ARGS}
